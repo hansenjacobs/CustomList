fd33046ac14b0201239396c46368e7bdfc9c0b3d
Jacob Hansen <hansenjacobs@gmail.com>
2018-05-10 15:38:59 -0500

    complete JList.Add() tests

--- CustomList/JList.cs
+++ CustomList/JList.cs
@@ -6,8 +6,61 @@ using System.Threading.Tasks;
 
 namespace CustomList
 {
-    class JList <T>
+    public class JList<T>
     {
+        static int capacityIncrement = 5;
+
+        T[] data;
+        int count;
+        int capacity;
+
+        public JList()
+        {
+            count = 0;
+            data = new T[5];
+            capacity = 5;
+        }
+
+        public int Capacity
+        {
+            get { return capacity; }
+        }
+
+        public static int CapacityIncrement
+        {
+            get { return capacityIncrement; }
+        }
+
+        public int Count
+        {
+            get { return count; }
+        }
+
+        public T this[int index]
+        {
+            get { return data[index]; }
+            private set { data[index] = value; }
+        }
+
+        public void Add(T value)
+        {
+
+        }
+
+        public void Insert(int index, T value)
+        {
+
+        }
+
+        public void Remove(T value)
+        {
+
+        }
+
+        public void RemoveAt(int index)
+        {
+
+        }
 
     }
 }
--- CustomListTests/JListTests.cs
+++ CustomListTests/JListTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomList;
 
 namespace CustomListTests
 {
@@ -7,8 +8,98 @@ namespace CustomListTests
     public class JListTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Add_AddItem_CountIncreasesByOne()
         {
+            // Arrange
+            JList<int> j = new JList<int>();
+
+            // Act
+            j.Add(10);
+
+            //Assert
+            int expected = 1;
+            int actual = j.Count;
+            Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void Add_AddItem_ItemAddedToEnd()
+        {
+            //Arrange
+            int expected = 20;
+            JList<int> j = new JList<int>();
+
+            //Act
+            j.Add(10);
+            j.Add(expected);
+
+            //Assert
+            int actual = j[1];
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Add_AddItem_CapacityIncreased()
+        {
+            //Arrange
+            JList<int> j = new JList<int>();
+            int itemsToAdd = 9;
+
+            //Act
+            for(int i = 0; i < itemsToAdd; i++)
+            {
+                j.Add(i);
+            }
+
+            //Assert
+            int expected = Convert.ToInt32(Math.Ceiling(Convert.ToDouble(itemsToAdd) / JList<int>.CapacityIncrement));
+            int actual = j.Capacity;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void RemoveAt_RemoveMiddleIndex_CountDecreasesByOne()
+        {
+            // Arrange
+            int itemsToAdd = 4;
+            JList<int> j = new JList<int>();
+
+            // Act
+            for(int i = 0; i < itemsToAdd; i++)
+            {
+                j.Add(i);
+            }
+
+            j.RemoveAt(1);
+
+            //Assert
+            int expected = itemsToAdd - 1;
+            int actual = j.Count;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void RemoveAt_RemoveMiddleIndex_IndexAfterRemoveEqualsIndexPlusOneBefore()
+        {
+            // Arrange
+            int itemsToAdd = 4;
+            int index = 1;
+            JList<int> j = new JList<int>();
+
+            // Act
+            for (int i = 0; i < itemsToAdd; i++)
+            {
+                j.Add(i);
+            }
+
+            int expected = j[index + 1];
+
+            j.RemoveAt(index);
+
+            //Assert
+            int actual = j[index];
+            Assert.AreEqual(expected, actual);
+        }
+
     }
 }
